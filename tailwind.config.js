83b4e37b4bba8bf0dd569983bf739232fca220ec
AdrianXDE <adricarav05@gmail.com>
2024-05-31 11:31:09 -0600

    Add files via upload
    yes

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+extend: {
+  colors:{
+    H:'#E2C7F9',
+    Olivia:'#9B96D0',
+    Lana:'#C39DC7',
+  },
+},
+
+  },
+  plugins: [],
+}
+
